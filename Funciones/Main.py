a469d9aaa68dc6c1bd4582240beb38db79d2021f
sperezp23 <sperezp23@gmail.com>
2025-01-03 09:41:49 -0500

    Update: Envolvente superior_inferior

--- Funciones/Main.py
+++ Funciones/Main.py
@@ -1,9 +1,10 @@
 # %% Funciones  
 from Funciones.Verificar_conexion import verificar_conexion
 from Funciones.Buscar_cometa import buscar_cometa
+from Funciones.Verificar_cometa import verificar_cometa
 from Funciones.Envolvente_superior import envolvente_superior
 from Funciones.Envolvente_inferior import envolvente_inferior
-from Funciones.Verificar_cometa import verificar_cometa
+from Funciones.Envolvente_superior_inferior import envolvente_superior_inferior
 
 # %% Main
 def main() -> None:
@@ -30,11 +31,8 @@ Ingrese aquí su elección: '''
     while (conectado_a_internet) and ( not(Terminar)):
 
         # Leer opcion elegida
-        while True:
-            if opcion_elegida in opciones:
-                break
-            else:
-                opcion_elegida = input(texto_opciones)
+        while opcion_elegida not in opciones:
+            opcion_elegida = input(texto_opciones)
 
         # Buscar cometa
         if opcion_elegida == '1':
@@ -60,10 +58,10 @@ Ingrese aquí su elección: '''
         # Graficar ambas curvas de luz interna y externa
         elif opcion_elegida == '4':
             nombre_cometa = input('\nIngrese el nombre del cometa que desea graficar o, "volver_menu" para regresar: ') #'C/2023 A3'
-
+            fecha_inicial = input('\nIngrese la fecha inicial para el análisis en el formato AAAA-MM-DD')
+            
             if nombre_cometa != 'volver_menu' and verificar_cometa(nombre_cometa, conectado_a_internet):
-                envolvente_superior(nombre_cometa, conectado_a_internet)
-                envolvente_inferior(nombre_cometa, conectado_a_internet)
+                envolvente_superior_inferior(nombre_cometa, fecha_inicial)
 
         # Finalizar programa
         elif opcion_elegida == '5':
