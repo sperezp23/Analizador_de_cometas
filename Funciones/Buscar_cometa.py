5fed03adaae70d7540ad88b31f68f32ebd5f4164
sperezp23 <sperezp23@gmail.com>
2025-01-03 10:37:48 -0500

    Update: 'verificar la conexión a internet de internet antes de buscar el cometa'.

--- Funciones/Buscar_cometa.py
+++ Funciones/Buscar_cometa.py
@@ -11,19 +11,21 @@ def buscar_cometa(cometa_buscado: str) -> None:
     '''
 
     conectado_a_internet = verificar_conexion()
-    cometas_hallados = []
-    content = conectar_con_API_de_COBS_Lista_de_Cometas(conectado_a_internet)
 
-    cometas_df = pd.DataFrame(content['objects'])
-    lista_cometas_df = cometas_df[['name', 'fullname']]
-    cometas_hallados =  lista_cometas_df.name[lista_cometas_df.fullname.str.contains(cometa_buscado)].values
+    if conectado_a_internet:
+        cometas_hallados = []
+        content = conectar_con_API_de_COBS_Lista_de_Cometas(conectado_a_internet)
 
-    if len(cometas_hallados) == 0:
-        print(f'🛑 No hay coincidencias para: {cometa_buscado}, en la base de datos (COBS).')
-    
-    else:
-        aux = '\n'*2
-        print(f'✅ Coincidencias halladas:{aux}{cometas_hallados}')
+        cometas_df = pd.DataFrame(content['objects'])
+        lista_cometas_df = cometas_df[['name', 'fullname']]
+        cometas_hallados =  lista_cometas_df.name[lista_cometas_df.fullname.str.contains(cometa_buscado)].values
+
+        if len(cometas_hallados) == 0:
+            print(f'🛑 No hay coincidencias para: {cometa_buscado}, en la base de datos (COBS).')
+        
+        else:
+            aux = '\n'*2
+            print(f'✅ Coincidencias halladas:{aux}{cometas_hallados}')
 
 if __name__ == '__main__':
     buscar_cometa()
