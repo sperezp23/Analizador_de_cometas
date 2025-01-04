deba4ec91a3e0fbb8f272e9894c83297d6641207
sperezp23 <sperezp23@gmail.com>
2025-01-04 12:21:49 -0500

    Update: 'Guardar las imágenes en formato png y html (para hacerlas interactivas).

--- Funciones/Curvas_de_luz_interna_externa.py
+++ Funciones/Curvas_de_luz_interna_externa.py
@@ -1,13 +1,21 @@
 # Gráfica de luz promediada
 import plotly.graph_objects as go
 
+# Funciones
+from Funciones.Crear_carpetas import crear_carpetas
+
 def curvas_de_luz_interna_externa(nombre_cometa, curva_de_luz_externa_df, curva_de_luz_interna_df):
+    titulo = f'Max/Min Averaged Lightcurve of comet {nombre_cometa}'
+    ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo)
+
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=curva_de_luz_externa_df.delta_t, y=curva_de_luz_externa_df.promedio_movil, mode='markers', name='Envolvente', marker=dict(color='yellow', line=dict(width=1, color='DarkSlateGrey'))))
     fig.add_trace(go.Scatter(x=curva_de_luz_interna_df.delta_t, y=curva_de_luz_interna_df.promedio_movil, mode='markers', name='Núcleo', marker=dict(color='red', line=dict(width=1, color='DarkSlateGrey'))))
     fig.update_layout(template='plotly_dark')
     fig.update_yaxes(autorange="reversed")
-    fig.update_layout(template='plotly_dark', xaxis_title='t-Δt', yaxis_title='Reduced Magnitude', title = f'Max/Min Averaged Lightcurve of comet {nombre_cometa}')
+    fig.update_layout(template='plotly_dark', xaxis_title='t-Δt', yaxis_title='Reduced Magnitude', title = titulo)
+    fig.write_image(ruta_archivos_graficas, scale= 10)
+    fig.write_html(ruta_archivos_graficas.replace('png', 'html'))
     fig.show()
 
     print('✅ Creada: curva de luz Interna-Externa.')
