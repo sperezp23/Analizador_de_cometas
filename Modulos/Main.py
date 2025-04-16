4b27577f13f19ee1547ab546690976524820b171
Santiago Pérez Puerta <sperezp23@gmail.com>
2025-04-16 13:30:55 -0500

    Update: 'Nueva opción creada, imprimir instrucciones en pantalla.'

--- Modulos/Main.py
+++ Modulos/Main.py
@@ -1,5 +1,6 @@
 # %% Funciones  
 from Modulos.Manejo_de_errores.Verificar_conexion import verificar_conexion
+from Modulos.Lectura_de_archivos.Leer_instrucciones import leer_instrucciones
 from Modulos.Manejo_de_errores.Buscar_cometa import buscar_cometa
 from Modulos.Procesamiento_de_datos.Envolvente_superior import envolvente_superior
 from Modulos.Procesamiento_de_datos.Envolvente_inferior import envolvente_inferior
@@ -13,13 +14,14 @@ def main() -> None:
 
     #  Variables
     opcion_elegida: str = '-1'
-    opciones : str = '12345'
+    opciones : str = 'i12345'
     mensaje_de_inicio: str = f'{'\n'*2}{'='*32}\n|| GENERADOR DE CURVAS DE LUZ ||\n{'='*32}'
-    texto_opciones : str  = f'''Elija una de las siguientes opciones:
-[1] : Buscar un comenta.
+    texto_opciones : str  = f'''Elija una de las siguientes opciones:\n
+[i] : Instrucciones (mostrar en pantalla las instrucciones del programa).\n
+[1] : Buscar el nombre de un cometa en la base de datos.
 [2] : Graficar curvas de luz externas (Envolvente superior).
 [3] : Graficar curvas de luz internas (Envolvente inferior).
-[4] : Graficar curvas de luz externa e internas (Envolvente inferior e inferior).
+[4] : Graficar curvas de luz externa e internas (Envolvente Exterior e inferior).
 [5] : Finalizar programa.\n
 Ingrese aquí su elección: '''
     
@@ -35,6 +37,10 @@ Ingrese aquí su elección: '''
         # Leer opcion elegida
         while opcion_elegida not in opciones:
             opcion_elegida = input(texto_opciones)
+
+        # Instrucciones
+        if opcion_elegida == 'i':
+            leer_instrucciones('Instrucciones.txt')
 
         # Buscar cometa
         if opcion_elegida == '1':
