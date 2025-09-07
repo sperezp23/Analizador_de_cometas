cb4d3ce22d11033d65498b9a690344253c1b2404
Santiago Pérez Puerta <sperezp23@gmail.com>
2025-09-07 11:45:27 -0500

    CREATE: 'generador de archivos, creador de carpetas.'

--- Modulos/Crear_carpetas/Crear_carpetas.py
+++ Modulos/Crear_carpetas/Crear_carpetas.py
@@ -3,13 +3,13 @@ import os
 from pathlib import Path
 from datetime import date
 
-def crear_carpetas(nombre_cometa, titulo):
+def crear_carpetas(nombre_cometa, nombre_archivo=None, carpeta_base='Graficas', tipo_archivo = 'png') -> str:
     fecha_actual = str(date.today())
-    carpeta_cometa = Path('Graficas', nombre_cometa.replace('/', '_'))
+    carpeta_cometa = Path(carpeta_base, nombre_cometa.replace('/', '_'))
     carpeta_fecha = Path(carpeta_cometa, fecha_actual)
 
-    if not os.path.exists('Graficas'):
-        Path.mkdir('Graficas')
+    if not os.path.exists(carpeta_base):
+        Path.mkdir(carpeta_base)
 
     if not os.path.exists(carpeta_cometa):
         Path.mkdir(carpeta_cometa)
@@ -17,8 +17,8 @@ def crear_carpetas(nombre_cometa, titulo):
     if not os.path.exists(carpeta_fecha):
         Path.mkdir(carpeta_fecha)
 
-    aux = titulo.replace('/', '_')
-    return f'{carpeta_fecha}/{aux}__{fecha_actual}.png'
+    aux = nombre_archivo.replace('/', '_')
+    return f'{carpeta_fecha}/{aux}__{fecha_actual}.{tipo_archivo}'
 
 if __name__ == '__main__':
     crear_carpetas()
---
+++ Modulos/Generar_archivos/Generar_archivos.py
@@ -0,0 +1,56 @@
+# Librerías
+from pandas import DataFrame
+
+# Funciones
+from Modulos.Manejo_de_errores.Verificar_conexion import verificar_conexion
+from Modulos.Manejo_de_errores.Verificar_cometa import verificar_cometa
+from Modulos.Manejo_de_errores.Verificar_fecha import verificar_fecha
+from Modulos.Coneccion_con_API.Conectar_con_API_de_COBS_Observaciones import conectar_con_API_de_COBS_Observaciones
+from Modulos.Procesamiento_de_datos.Tratamiento_de_datos_cometa import tratamiento_de_datos_cometa
+from Modulos.Coneccion_con_API.Conectar_con_API_de_MPC_Efemerides import descargar_efemerides
+from Modulos.Coneccion_con_API.Obtener_perihelio import obtener_perihelio
+from Modulos.Procesamiento_de_datos.Tratamiento_de_datos_con_efemerides import tratamiento_de_datos_con_efemerides
+from Modulos.Crear_carpetas.Crear_carpetas import crear_carpetas
+
+def generar_archivos(nombre_cometa: str, fecha_inicial: str, conectado_a_internet: bool) -> None:
+    '''
+    Procesa los datos del cometa especificado, para calcular la 
+    curva de luz reducida y retornar el archivo con los datos respectivos.
+    '''
+
+    # Verificar cometa en la base de datos y conexión a internet
+    if verificar_conexion() and verificar_cometa(nombre_cometa, conectado_a_internet) and verificar_fecha(fecha_inicial):
+    
+        # Conexión con la API de COBS
+        content = conectar_con_API_de_COBS_Observaciones(nombre_cometa, fecha_inicial, conectado_a_internet)
+
+        # Tratamiento de datos observacionales
+        curva_de_luz_cruda_df = tratamiento_de_datos_cometa(content)
+
+        # Descargar efemérides
+        efemerides_df = descargar_efemerides(nombre_cometa, curva_de_luz_cruda_df)
+
+        # Obtener perihelio de la API de COBS
+        perihelio = obtener_perihelio(nombre_cometa, conectado_a_internet)
+
+        # Tratamiento de datos con efemerides
+        curva_de_luz_procesada_df = tratamiento_de_datos_con_efemerides(curva_de_luz_cruda_df, efemerides_df, perihelio)
+
+        # Generar archivos txt
+        archivo_curva_de_luz_procesada_df = DataFrame()
+        archivo_curva_de_luz_procesada_df['Year'] = curva_de_luz_procesada_df.obs_date.dt.year
+        archivo_curva_de_luz_procesada_df['Month'] = curva_de_luz_procesada_df.obs_date.dt.month
+        archivo_curva_de_luz_procesada_df['Day'] = curva_de_luz_procesada_df.obs_date.dt.day
+        archivo_curva_de_luz_procesada_df['t-Tq'] = curva_de_luz_procesada_df.delta_t
+        archivo_curva_de_luz_procesada_df['delta'] = curva_de_luz_procesada_df.delta
+        archivo_curva_de_luz_procesada_df['r'] = curva_de_luz_procesada_df.r
+        archivo_curva_de_luz_procesada_df['alpha'] = curva_de_luz_procesada_df.phase
+        archivo_curva_de_luz_procesada_df['magnitud_cruda'] = curva_de_luz_procesada_df.magnitude
+        archivo_curva_de_luz_procesada_df['magnitud_reducida_delta'] = curva_de_luz_procesada_df.magnitud_reducida
+        archivo_curva_de_luz_procesada_df['magnitud_reducida_alpha'] = curva_de_luz_procesada_df.magnitud_reducida
+
+        ruta_archivos = crear_carpetas(nombre_cometa, nombre_archivo = 'Datos_procesados_cometa_', carpeta_base='Bases_de_datos', tipo_archivo= 'txt')
+        archivo_curva_de_luz_procesada_df.to_csv(ruta_archivos, index=False, sep='\t')
+
+if __name__ == '__main__':
+    generar_archivos()
